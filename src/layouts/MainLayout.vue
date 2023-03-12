33b9269770360124948923587a20e687019b7b68
luoboQAQ <qqhistory@126.com>
2023-03-12 17:31:30 +0800

    小写一波样式

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,21 +1,26 @@
 <template>
   <n-grid x-gap="12">
     <n-gi span="6">
-      <n-card :bordered="false" title="系统状态" class="light-green">
-        <p>当前在线： 1</p>
-        <p>服务总数： 10</p>
-        <p v-for="server in servers" :key="server.name">
-          {{ server.name }}: <n-icon :component="server.online ? CloudCheckmark16Regular : CloudDismiss16Regular"
-            size="40" />
-        </p>
+      <n-card title="系统状态" :segmented="{ content: true, footer: 'soft' }">
+        <p class="status">当前在线： {{ onlineNum }}</p>
+        <p class="status">服务总数： {{ servers.length }}</p>
+        <template #footer>
+          <p v-for="server in servers" :key="server.name" class="detail">
+            {{ server.name }}: &nbsp;&nbsp;&nbsp;&nbsp;
+            <n-icon :component="server.online ? CloudCheckmark16Regular : CloudDismiss16Regular"
+              :color="server.online ? '#18a058' : '#d03050'" size="40" />
+          </p>
+        </template>
       </n-card>
     </n-gi>
     <n-gi span="12">
-      <div class="green">
+      <n-gradient-text :size="60" type="success" class="title">
         基于Nginx的负载均衡管理界面
-      </div>
+      </n-gradient-text>
       <n-card>
-        当前模式： 轮询
+        <p style="font-size: 20px;">
+          当前模式： {{ mode }}
+        </p>
       </n-card>
       <n-card title="模式切换">
         <n-radio-group v-model="mode">
@@ -37,39 +42,52 @@
     </n-gi>
     <n-gi span="6">
       <n-log :log="`1
-          2
-          3
-          4
-                5
-                6`" />
+                2
+                      3
+                      4
+                            5
+                                  6`" />
     </n-gi>
   </n-grid>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import { CloudCheckmark16Regular, CloudDismiss16Regular } from '@vicons/fluent'
 
 const servers = ref([
-  { name: 'server1', online: true },
-  { name: 'server2', online: false },
-  { name: 'server3', online: true },
-  { name: 'server4', online: false },
-  { name: 'server5', online: true },
-  { name: 'server6', online: false },
-  { name: 'server7', online: true },
-  { name: 'server8', online: false },
-  { name: 'server9', online: true }])
+  { name: '服务1', online: true },
+  { name: '服务2', online: true },
+  { name: '服务3', online: true },
+  { name: '服务4', online: false },
+  { name: '服务5', online: false },
+  { name: '服务6', online: false },
+  { name: '服务7', online: true },
+  { name: '服务8', online: false },
+  { name: '服务9', online: true }])
+
+const mode = ref('轮询')
+
+const onlineNum = computed(() => servers.value.filter(server => server.online).length)
 
 </script>
 
 <style scoped>
-.light-green {
-  background-color: rgba(0, 128, 0, 0.12);
+.title {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 300px;
 }
 
-.green {
-  height: 108px;
-  background-color: rgba(0, 128, 0, 0.24);
+.detail {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 20px;
+}
+
+.status {
+  font-size: 30px;
 }
 </style>
