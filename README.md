a4134e5e114b083ee46d5089ffc8e74c5a1e43e6
luoboQAQ <qqhistory@126.com>
2023-03-12 16:07:07 +0800

    搭好页面框架

--- README.md
+++ README.md
@@ -1,35 +1,3 @@
-# nginx-lb
+# nginx负载均衡管理页面
 
-This template should help get you started developing with Vue 3 in Vite.
-
-## Recommended IDE Setup
-
-[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur) + [TypeScript Vue Plugin (Volar)](https://marketplace.visualstudio.com/items?itemName=Vue.vscode-typescript-vue-plugin).
-
-## Customize configuration
-
-See [Vite Configuration Reference](https://vitejs.dev/config/).
-
-## Project Setup
-
-```sh
-npm install
-```
-
-### Compile and Hot-Reload for Development
-
-```sh
-npm run dev
-```
-
-### Compile and Minify for Production
-
-```sh
-npm run build
-```
-
-### Lint with [ESLint](https://eslint.org/)
-
-```sh
-npm run lint
-```
+该项目是为了熟悉Vue3 + ~~完成任务~~而写的，属于是练手作品，写的比较烂QAQ。
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -4,21 +4,52 @@
       <n-card :bordered="false" title="系统状态" class="light-green">
         <p>当前在线： 1</p>
         <p>服务总数： 10</p>
+        <p v-for="server in servers" :key="server.name">
+          {{ server.name }}: <n-icon :component="server.online ? CloudCheckmark16Regular : CloudDismiss16Regular"
+            size="40" />
+        </p>
       </n-card>
     </n-gi>
     <n-gi span="12">
       <div class="green">
         基于Nginx的负载均衡管理界面
       </div>
+      <n-card>
+        当前模式： 轮询
+      </n-card>
+      <n-card title="模式切换">
+        <n-radio-group v-model="mode">
+          <n-radio label="轮询">轮询</n-radio>
+          <n-radio label="权重">权重</n-radio>
+          <n-radio label="IP">IP</n-radio>
+        </n-radio-group>
+        <n-checkbox-group v-model="mode">
+          <n-checkbox v-for="server in servers" :key="server.name" :label="server.name">
+            {{ server.name }}
+            <n-input-number v-model="server.weight" :min="1" :max="10" />
+          </n-checkbox>
+        </n-checkbox-group>
+        <n-button type="primary">
+          提交
+        </n-button>
+        <n-button>清空</n-button>
+      </n-card>
     </n-gi>
     <n-gi span="6">
-      <div class="light-green" />
+      <n-log :log="`1
+          2
+          3
+          4
+                5
+                6`" />
     </n-gi>
   </n-grid>
 </template>
 
 <script setup>
 import { ref } from 'vue'
+import { CloudCheckmark16Regular, CloudDismiss16Regular } from '@vicons/fluent'
+
 const servers = ref([
   { name: 'server1', online: true },
   { name: 'server2', online: false },
@@ -28,8 +59,7 @@ const servers = ref([
   { name: 'server6', online: false },
   { name: 'server7', online: true },
   { name: 'server8', online: false },
-  { name: 'server9', online: true },
-  { name: 'server10', online: false }])
+  { name: 'server9', online: true }])
 
 </script>
 
